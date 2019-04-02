f76b2f11efcba24b57485fe6391f80d522cfeaa1
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-04-02 08:20:21 -0700

    [DVD] Optimized file output for ngramFT.py

--- DVD/ngramFT.py
+++ DVD/ngramFT.py
@@ -3,6 +3,7 @@ import re
 import sys
 
 frequencies = {}
+one_occurence = []
 
 args = sys.argv
 
@@ -39,14 +40,17 @@ half_c_len = c_len // 2
 for n_to_count in range(1, half_c_len + 1):
     print("Counting strings of length", n_to_count)
     for ind in range(0, c_len - (n_to_count - 1)):
-        string_to_count = content[ind:ind + n_to_count + 1]
+        string_to_count = content[ind:ind + n_to_count]
 
         if string_to_count in frequencies:
             continue
 
         matches = re.findall(string_to_count, content)
-    
-        frequencies[string_to_count] = len(matches)
+
+        if len(matches) > 1:
+            frequencies[string_to_count] = len(matches)
+        else:
+            one_occurence.append(string_to_count)
 
 fname = "ngram-results-"
 
@@ -66,6 +70,16 @@ except FileExistsError:
     exit()
 
 with open(fname, "w") as fp:
-    fp.write(str(frequencies))
+    for k in frequencies:
+        fp.write(k + ": " + str(frequencies[k]) + "\n")
+    if len(one_occurence) == 0:
+        pass
+    elif len(one_occurence) == 1:
+        fp.write(one_occurence[0] + "appeared only once.\n")
+        pass
+    else:
+        fp.write("\nThe following strings occured only once:\n")
+        for occ in one_occurence:
+            fp.write(occ + "\n")
 
-print("Output written to", fname, "in JSON/Python dictionary format.")
+print("Output written to", fname)
